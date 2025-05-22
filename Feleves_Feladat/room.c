dca0dd734daa254db616d6b21662826a84ac8c1f
BLaszlo <laszlo.bodnarxd@gmail.com>
2025-05-22 10:46:08 +0200

    jobb feny a falakon

--- Feleves_Feladat/room.c
+++ Feleves_Feladat/room.c
@@ -1,10 +1,12 @@
 #include "room.h"
 
 void drawRoom(GLuint floorTex, GLuint wallTex) {
+    glColor3f(1.0f, 1.0f, 1.0f);
+
 
     glBindTexture(GL_TEXTURE_2D, floorTex);
-    glColor3f(1.0f, 1.0f, 1.0f);
     glBegin(GL_QUADS);
+        glNormal3f(0.0f, 1.0f, 0.0f);
         glTexCoord2f(0, 0); glVertex3f(-5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 0); glVertex3f(5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 5); glVertex3f(5.0f, 0.0f, 5.0f);
@@ -13,10 +15,10 @@ void drawRoom(GLuint floorTex, GLuint wallTex) {
 
 
     glBindTexture(GL_TEXTURE_2D, wallTex);
-    glColor3f(1.0f, 1.0f, 1.0f);
 
 
     glBegin(GL_QUADS);
+        glNormal3f(0.0f, 0.0f, 1.0f);
         glTexCoord2f(0, 0); glVertex3f(-5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 0); glVertex3f(5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 3); glVertex3f(5.0f, 3.0f, -5.0f);
@@ -25,6 +27,7 @@ void drawRoom(GLuint floorTex, GLuint wallTex) {
 
 
     glBegin(GL_QUADS);
+        glNormal3f(-1.0f, 0.0f, 0.0f);
         glTexCoord2f(0, 0); glVertex3f(5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 0); glVertex3f(5.0f, 0.0f, 5.0f);
         glTexCoord2f(5, 3); glVertex3f(5.0f, 3.0f, 5.0f);
@@ -33,6 +36,7 @@ void drawRoom(GLuint floorTex, GLuint wallTex) {
 
 
     glBegin(GL_QUADS);
+        glNormal3f(0.0f, 0.0f, -1.0f);
         glTexCoord2f(0, 0); glVertex3f(5.0f, 0.0f, 5.0f);
         glTexCoord2f(5, 0); glVertex3f(-5.0f, 0.0f, 5.0f);
         glTexCoord2f(5, 3); glVertex3f(-5.0f, 3.0f, 5.0f);
@@ -41,20 +45,21 @@ void drawRoom(GLuint floorTex, GLuint wallTex) {
 
 
     glBegin(GL_QUADS);
+        glNormal3f(1.0f, 0.0f, 0.0f);
         glTexCoord2f(0, 0); glVertex3f(-5.0f, 0.0f, 5.0f);
         glTexCoord2f(5, 0); glVertex3f(-5.0f, 0.0f, -5.0f);
         glTexCoord2f(5, 3); glVertex3f(-5.0f, 3.0f, -5.0f);
         glTexCoord2f(0, 3); glVertex3f(-5.0f, 3.0f, 5.0f);
     glEnd();
 
-    glBindTexture(GL_TEXTURE_2D, wallTex);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(-5.0f, 3.0f, -5.0f);
-    glTexCoord2f(5, 0); glVertex3f(5.0f, 3.0f, -5.0f);
-    glTexCoord2f(5, 5); glVertex3f(5.0f, 3.0f, 5.0f);
-    glTexCoord2f(0, 5); glVertex3f(-5.0f, 3.0f, 5.0f);
-    glEnd();
 
+    glBegin(GL_QUADS);
+        glNormal3f(0.0f, -1.0f, 0.0f);
+        glTexCoord2f(0, 0); glVertex3f(-5.0f, 3.0f, -5.0f);
+        glTexCoord2f(5, 0); glVertex3f(5.0f, 3.0f, -5.0f);
+        glTexCoord2f(5, 5); glVertex3f(5.0f, 3.0f, 5.0f);
+        glTexCoord2f(0, 5); glVertex3f(-5.0f, 3.0f, 5.0f);
+    glEnd();
 
     glColor3f(1.0f, 1.0f, 1.0f);
 }
